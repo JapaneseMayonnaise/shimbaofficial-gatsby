35fc0413c4ed17dc884353c763135258323265d0
DrCardamom <erkshimba@gmail.com>
2020-05-14 13:08:48 -0400

    add manifest plugin and offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   siteMetadata: 
   {
-    title: `Elliot Shimba`
+    title: `Elliot Shimba`,
+    description: `Elliot Shimba's portfolio website`,
+    author: `Elliot Shimba`,
   },
   plugins: 
   [
@@ -13,12 +15,22 @@ module.exports = {
       },
     },
     `gatsby-plugin-sass`,
-    // {
-    //   resolve: `gatsby-plugin-typography`,
-    //   options: {
-    //     pathToConfigModule: `src/utils/typography`,
-    //   },
-    // },
     `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Elliot Shimba`,
+        short_name: `Shimba`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#00D1B2`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
   ],
 }
