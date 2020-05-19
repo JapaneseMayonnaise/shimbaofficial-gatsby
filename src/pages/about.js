352687f5a82bd5cb805bb2fa1550d1fdb193cf6c
DrCardamom <erkshimba@gmail.com>
2020-05-19 11:45:30 -0400

    fixed graphql query to render multiple images by aliasing

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { graphql } from "gatsby"
+import Img from "gatsby-image"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
-import Img from "gatsby-image"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
 import style from "./about.module.css"
 import "../../sass/mystyles.scss"
 
 export default function About({data}) {
+   
    return (
       <Layout>
          <SEO title="About" /> 
@@ -19,58 +20,49 @@ export default function About({data}) {
                   </h1>
                   <div className="columns is-multiline">
                      <div className={`column is-two-thirds ${style.debugger}`}>
-                        <h2 className="title is-size-3 is-size-4-touch has-text-less-intense">
+                        <h2 className="title is-size-3 is-size-4-touch">
                            A Toronto-based full-stack web developer `#:computer:`, <br />
                            strong advocate of diversity &amp; inclusion in tech community :rainbow:, <br />
                            avid sourdough-baker :bread:
                         </h2>
                      </div>
                      <div className={`column is-one-third ${style.debugger}`}>
-                        <p className="image is-128x128">
-                           Photo 1
                            <Img
-                              fluid={data.file.childImageSharp.fluid}
+                              fluid={data.firstImage.childImageSharp.fluid}
                               alt="shimba's profile photo first of four"
                            />
-                        </p>
                      </div>   
                      <div className={`column is-two-thirds ${style.debugger}`}>
-                        <h2 className="is-size-4 is-size-5-touch has-text-less-intense">
+                        <h2 className="is-size-4 is-size-5-toeuch">
                            JavaScript is my favourite language and I strive to write clean, reusable code. <br />
-                           I enjoy crafting beautiful full-stack applications with JavaScript and JS-fueled tools 
-                           such as React.js, Node.js, TypeScript, Redux.js, Gatsby, PostgreSQL, MongoDB, and more.(I never stop learning!)
+                           I enjoy crafting beautiful full-stack applications with JavaScript and JS-fueled / related tools 
+                           such as React.js, Node.js, TypeScript, Redux.js, Gatsby, PostgreSQL, MongoDB, Jest, Git and more.(I never stop learning!)
                         </h2>
                      </div>
                      <div className={`column is-one-third ${style.debugger}`}>
-                        <p className="image is-128x128">
-                           Photo 2
                            <Img
-                              fluid={data.file.childImageSharp.fluid}
+                              fluid={data.secondImage.childImageSharp.fluid}
                               alt="shimba's profile photo first of four"
                            />
-                        </p>
                      </div>
                      <div className={`column is-two-thirds ${style.debugger}`}>
-                        <h2 className="is-size-4 is-size-5-touch has-text-less-intense">
-                           Besides web development, things I've been keen on are;
+                        <h2 className="is-size-4 is-size-5-touch">
+                           Besides web development, things I've been keen on include
+                        </h2>
                            <h3 className="is-size-4 is-size-5-touch">
                               Operating systems, open-source projects(even better if supporting or mainly run by queer / people of colour), teaching kids how to code, 
-                              anti-surveillance movement, net neutrality.<br /> 
-                              Same? Great! Let's make it heppen together or let me join your project!
+                              anti-surveillance, online privacy, net neutrality.<br /> 
                            </h3>
-                        </h2>
                      </div>
                      <div className={`column is-one-third ${style.debugger}`}>
-                        <p className="image is-128x128">
-                           Photo 3
                            <Img
-                              fluid={data.file.childImageSharp.fluid}
+                              fluid={data.thirdImage.childImageSharp.fluid}
                               alt="shimba's profile photo first of four"
-                           />
-                        </p>
+                              />
                      </div>
                      <div className={`column is-two-thirds ${style.debugger}`}>
-                       <h2 className="is-size-4 is-size-5-touch has-text-less-intense">
+                       <h2 className="is-size-4 is-size-5-touch">
+                           Oh, you same? Excellent! Let's make it heppen together or let me contribute to your project!
                            To get in touch, you can
                            <a href="mailto:shimba@posteo.net"> email me </a>
                            or message me on
@@ -82,13 +74,10 @@ export default function About({data}) {
                         </h2>
                      </div>
                      <div className={`column is-one-third ${style.debugger}`}>
-                        <p className="image is-128x128">
-                           Photo 4
                            <Img
-                              fluid={data.file.childImageSharp.fluid}
+                              fluid={data.fourthImage.childImageSharp.fluid}
                               alt="shimba's profile photo first of four"
                            />
-                        </p>
                      </div>
                   </div>
                </div>
@@ -99,14 +88,33 @@ export default function About({data}) {
 
 export const query = graphql`
     query {
-      file(relativePath: { eq: "images/profile_photo_1.jpg" }) {
+      firstImage: file(relativePath: { eq: "images/profile_photo_lookingStraight.jpg" }) {
         childImageSharp {
-          # Specify a fixed image and fragment.
-          # The default width is 400 pixels
-          fluid (maxWidth: 1600){
+          fluid (maxWidth: 800){
             ...GatsbyImageSharpFluid
           }
         }
-      }
+      },
+      secondImage: file(relativePath: { eq: "images/profile_photo_lookingLeft.jpg" }) {
+         childImageSharp {
+           fluid (maxWidth: 800){
+             ...GatsbyImageSharpFluid
+           }
+         }
+       },
+       thirdImage: file(relativePath: { eq: "images/profile_photo_lookingRight.jpg" }) {
+         childImageSharp {
+           fluid (maxWidth: 800){
+             ...GatsbyImageSharpFluid
+           }
+         }
+       },
+       fourthImage: file(relativePath: { eq: "images/profile_photo_lookingStraight.jpg" }) {
+         childImageSharp {
+           fluid (maxWidth: 800){
+             ...GatsbyImageSharpFluid
+           }
+         }
+       }
     }
   `
