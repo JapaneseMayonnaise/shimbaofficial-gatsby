4845dbecd52a6844e5c6cc29b42571540578ab35
DrCardamom <erika.shimba@400f.jp>
2022-01-31 11:36:08 +0100

    Only show technical growth log for now

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,43 +3,48 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 
 // create a slug for each pages
 exports.onCreateNode = ({ node, getNode, actions }) => {
-   const { createNodeField } = actions
-   if(node.internal.type === `MarkdownRemark`) {
-      const slug = createFilePath({ node, getNode, basePath:`pages/posts`})
-      createNodeField({
-         node,
-         name:`slug`,
-         value: slug
-      })
-   }
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages/posts` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
 }
 
 // create a page that shows a md file using a slug
-exports.createPages = async({ graphql, actions }) => {
-   const { createPage } = actions 
-   const result = await graphql(`
-      query {
-         allMarkdownRemark {
-            edges {
-               node {
-                  fields {
-                     slug
-                  }
-               }
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage, createRedirect } = actions
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
             }
-         }
-      } 
-   `)
-   
-   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-         path: node.fields.slug,
-         component: path.resolve(`./src/templates/growthLogTemplate.js`),
-         // you can use data passed to context in page query 
-         // as graphql variables like $slug like I did in growth-log.js
-         context: {
-            slug: node.fields.slug,
-         }
-      })
-   })
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/growthLogTemplate.js`),
+      // you can use data passed to context in page query
+      // as graphql variables like $slug like I did in growth-log.js
+      context: {
+        slug: node.fields.slug,
+      },
+    })
+  })
+
+  createRedirect({
+    fromPath: `/about`,
+    toPath: `/`,
+  })
 }
