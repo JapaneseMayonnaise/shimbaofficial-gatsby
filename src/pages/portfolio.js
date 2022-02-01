e722aec30e654d9b7e49847fd8a9bb1c708cbf8b
DrCardamom <erika.shimba@400f.jp>
2022-02-01 09:50:30 +0100

    Change layout

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,9 +1,9 @@
 import React from "react"
 import Showcase from "../components/showcase/showcase"
-import LatestPosts from "../components/latestPosts/latestPosts"
-import Layout from "../components/layout"
+import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 import "../../sass/mystyles.scss"
+import style from "./portfolio.module.css"
 
 export default function Portfolio() {
   return (
